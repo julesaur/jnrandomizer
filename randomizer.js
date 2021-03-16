1340975600ce42a4f425054181f87460eafa0026
julesaur <julee.nhek@sjsu.edu>
2021-03-15 20:31:35 -0700

    D:

--- randomizer.js
+++ randomizer.js
@@ -1,12 +1,13 @@
 //wish you were here with me
 
-let owl = ["owlena", "seer", "waves", "moon", "gem", "hammy"];
+let owl = [];
 
   function setup() {
   createCanvas(600, 600);
   background(220);
 
   owl[1] = "owlena eyes";
+
   console.log(owl);
 
 
